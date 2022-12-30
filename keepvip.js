158e1231c04bf6aadc9ecdc6b9027648266b3313
LittleXingzzZ <55964606+LittleXingzzZ@users.noreply.github.com>
2022-12-30 11:57:47 +0800

    Delete keepvip.js

--- keepvip.js
+++
@@ -1,4 +0,0 @@
-# keep解锁会员    
-hostname = *.gotokeep.com,api.keepkeep.com
-
-^https://(.+).gotokeep.com(/athena/v5/people/my|/nuocha/plans) url script-response-body https://raw.githubusercontent.com/LittleXingzzZ/QuantumultxxX/main/Script/keepvip.js
